14cb41672fd333d9714aab76d10324b452e96dec
monsterkodi <monsterkodi@gmx.net>
2025-08-22 02:15:20 +0200

    materials

--- td/ui/PauseMenu.gd
+++ td/ui/PauseMenu.gd
@@ -1,10 +1,20 @@
 extends Control
 
 signal resumeGame
+signal loadGame
+signal saveGame
 signal quitGame
 
 func _on_quit_button_pressed():   quitGame.emit()
 func _on_resume_button_pressed(): resumeGame.emit()
+func _on_load_pressed():          loadGame.emit()
+func _on_save_pressed():          saveGame.emit()
+
+func _input(event: InputEvent):
+    
+    if Input.is_action_just_pressed("ui_cancel"): # to enable joypad B
+        get_viewport().set_input_as_handled()
+        resumeGame.emit()
 
 func _on_visibility_changed():
     if is_visible_in_tree():
